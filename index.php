87dc4d20944868df16bf65af90595d3003ca6c8f
Casper School <casperschool10@gmail.com>
2018-11-17 17:32:40 +0100

    update

--- index.php
+++ index.php
@@ -9,18 +9,14 @@
     <link rel="stylesheet" href="css/theme.css">
 </head>
     <body>
-    <?php
-        $name = "";
-        $answer1_1 = $answer1_2 = $answer2_1 = $answer2_2 = $answer2_3 = $answer2_4 = $answer2_5 = $answer2_6 = $answer2_7 = 0;
-    ?>
         <div id="fullpage">
             <div class="fp-section" class="active">
                 <div class="all">
-                    <p id="changeText" onclick="changeText()">welkom!</p>
+                    <p id="changeText" onclick="changeText()">Welkom!</p>
                         <div class="form"> 
                             <label for="name"> Vul hier uw naam in: </label><br/>
-                            <input type="text" name="name" placeholder="Name" required><br/>
-                            <button value="Begin" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDownEcht()">Begin</button>
+                            <input type="text" id="name" name="name" placeholder="Name" required><br/>
+                            <button value="Begin" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown('name')">Begin</button>
                         </div>
                 </div>
             </div>
@@ -31,18 +27,10 @@
 
                 <div class="all">
                     <p>werkt het inklok systeem altijd goed?</p>
-                        <div class="answers">  
-                            <button id="answer1_1" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">nee.</button>
-                            <button id="answer1_2" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">helemaal kut.</button>
-                        </div>
-                        <?php
-                            if (isset($_GET["answer1_1"])) {  
-                                $answer1_1 = $_GET["answer1_1"];
-                            }
-                            else if (isset($_GET["answer1_2"])) {
-                                $answer1_2 = $_GET["answer1_2"];
-                            }
-                        ?>
+                    <div class="answers">  
+                        <button id="answer1_1" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(0)">nee.</button>
+                        <button id="answer1_2" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(1)">helemaal kut.</button>
+                    </div>
                 </div>
             </div>
             <div class="fp-section">
@@ -54,37 +42,14 @@
                     <p>Wie is de chillste leraar?</p> 
                     
                     <div class="answers">
-                        <button id="answer2_1" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Monique Moeskops</button>
-                        <button id="answer2_2" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Pierre Ruijters</button>
-                        <button id="answer2_3" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Anneke Bouma</button>
-                        <button id="answer2_4" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Francy Verwaard</button>
-                        <button id="answer2_5" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Bouke Loman</button>
-                        <button id="answer2_6" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Stef Heiltjes</button>
-                        <button id="answer2_7" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Koos Zagwijn</button>  
+                        <button id="answer2_1" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(0)">Monique Moeskops</button>
+                        <button id="answer2_2" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(1)">Pierre Ruijters</button>
+                        <button id="answer2_3" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(2)">Anneke Bouma</button>
+                        <button id="answer2_4" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(3)">Francy Verwaard</button>
+                        <button id="answer2_5" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(4)">Bouke Loman</button>
+                        <button id="answer2_6" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(5)">Stef Heiltjes</button>
+                        <button id="answer2_7" data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(6)">Koos Zagwijn</button>  
                     </div>  
-                        <?php
-                            if (isset($_GET["answer2_1"])) {  
-                                $answer2_1 = $_GET["answer2_1"];
-                            }
-                            else if (isset($_GET["answer2_2"])) {
-                                $answer2_2 = $_GET["answer2_2"];
-                            }
-                            else if (isset($_GET["answer2_3"])) {
-                                $answer2_3 = $_GET["answer2_3"];
-                            }
-                            else if (isset($_GET["answer2_4"])) {
-                                $answer2_4 = $_GET["answer2_4"];
-                            }
-                            else if (isset($_GET["answer2_5"])) {
-                                $answer2_5 = $_GET["answer2_5"];
-                            }
-                            else if (isset($_GET["answer2_6"])) {
-                                $answer2_6 = $_GET["answer2_6"];
-                            }
-                            else if (isset($_GET["answer2_7"])) {
-                                $answer2_7 = $_GET["answer2_7"];
-                            }
-                        ?>
                 </div> 
             </div>
             <div class="fp-section">
@@ -96,8 +61,8 @@
                     <p>Wat vind je van de stagaire?</p> 
 
                     <div class="answers">  
-                        <button data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Goed</button>
-                        <button data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown()">Slecht</button>
+                        <button data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(0,true)">Goed</button>
+                        <button data-tilt data-tilt-glare data-tilt-max-glare="0.8" data-tilt-max="25" class="btnAnswer" onclick="moveDown(1,true)">Slecht</button>
                     </div>  
                 </div> 
             </div>
--- js/fade.js
+++ js/fade.js
@@ -1,13 +1,3 @@
-var answer1_1 = 0;
-var answer1_2 = 0;
-var answer2_1 = 0;
-var answer2_2 = 0;
-var answer2_3 = 0;
-var answer2_4 = 0;
-var answer2_5 = 0;
-var answer2_6 = 0;
-var answer2_7 = 0;
-
 var fp = new fullpage('#fullpage', {
 	autoScrolling: true,
 	scrollHorizontally: true
@@ -16,58 +6,45 @@ var fp = new fullpage('#fullpage', {
 fp.setMouseWheelScrolling(false)
 fp.setAllowScrolling(false)
 
-function moveDown () {
-    document.getElementById('answer1_1').onclick = function() {
-        answer1_1 = 1
-        window.location.href = "index.php?answer1_1=" + answer1_1
+var user = ""
+var answers = []
+var index = 0
+
+function moveDown(a,submit) {
+    if (document.getElementById("name").value !== "") {
+        if (a === "name") {
+            user = document.getElementById("name").value
+        }else{
+            answers.push({ question:index,answer:a})
+        }
+        index++
         fp.moveSectionDown()
-    }
-    document.getElementById('answer1_2').onclick = function() {
-        answer1_2 = 1  
-        window.location.href = "index.php?answer1_2=" + answer1_2
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_1').onclick = function() {
-        answer2_1 = 1  
-        window.location.href = "index.php?answer2_1=" + answer2_1
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_2').onclick = function() {
-        answer2_2 = 1  
-        window.location.href = "index.php?answer2_2=" + answer2_2
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_3').onclick = function() {
-        answer2_3 = 1  
-        window.location.href = "index.php?answer2_3=" + answer2_3
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_4').onclick = function() {
-        answer2_4 = 1  
-        window.location.href = "index.php?answer2_4=" + answer2_4
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_5').onclick = function() {
-        answer2_5 = 1  
-        window.location.href = "index.php?answer2_5=" + answer2_5
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_6').onclick = function() {
-        answer2_6 = 1  
-        window.location.href = "index.php?answer2_6=" + answer2_6
-        fp.moveSectionDown()
-    }
-    document.getElementById('answer2_7').onclick = function() {
-        answer2_7 = 1  
-        window.location.href = "index.php?answer2_7=" + answer2_7
-        fp.moveSectionDown()
+        if (submit) {
+            fetch("formHandeling.php", {
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: {
+                    name: user,
+                    answers: JSON.stringify(answers)
+                }
+            }).then(function (r) { return r.text() }).then(function (r) {
+                console.log(r)
+            })
+        }
     }
 }
 
-function moveDownEcht() {
-    fp.moveSectionDown()
-}
-
-function moveUp () {
+function moveUp() {
+    answers = answers.filter(function(a) {
+        return !(a.question === index || a.question === index-1)
+    })
+    index--
     fp.moveSectionUp()
+}
+
+function submit() {
+    console.log(answers)
+
 }
